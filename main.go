bc4ee8895de65189a311b051fea6c0feb44d958f
erkin97 <matkaziev.97@gmail.com>
2018-12-31 02:40:56 +0900

    remove all tags and entites

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 	"fmt"
 	"bufio"
 //	"io/ioutil"
+	"html"
 	"log"
 	"path/filepath"
+	"github.com/grokify/html-strip-tags-go"
 	"net/http"
 	"os"
 	"encoding/json"
@@ -33,16 +35,8 @@ func doQuote() string {
 	}
 	fmt.Printf("Your quote data %v", quoteData)
 	quoteContent := quoteData[0].(map[string]interface{})["content"].(string)
-	/*remove html tags manually*/
-	quoteContent = strings.Replace(quoteContent, "<p>", "", -1)
-	quoteContent = strings.Replace(quoteContent, "</p>", "", -1)
-	/*replace html entities some popular*/
-	quoteContent = strings.Replace(quoteContent, "&#8217;", "'", -1)
-	quoteContent = strings.Replace(quoteContent, "&#38;", "&", -1)
-	quoteContent = strings.Replace(quoteContent, "&#34;", "\"", -1)
-	quoteContent = strings.Replace(quoteContent, "&#8211;", "-", -1)
-	quoteContent = strings.Replace(quoteContent, "&#37;", "%", -1)
-	/*some editing*/
+	quoteContent = strip.StripTags(quoteContent)
+	quoteContent = html.UnescapeString(quoteContent)
 	quoteContent = strings.TrimSuffix(quoteContent, "\n")
 	quoteContent = strings.TrimSuffix(quoteContent, " ")
 	quoteAuthor := quoteData[0].(map[string]interface{})["title"].(string)
