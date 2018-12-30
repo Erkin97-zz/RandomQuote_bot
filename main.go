cb8b8ea87240dbb6ac3f29538b55de0604bb2319
erkin97 <matkaziev.97@gmail.com>
2018-12-31 02:12:13 +0900

    handle quote command

--- main.go
+++ main.go
@@ -2,21 +2,46 @@
 simple webhook server for my bot
 this spaghetti code just for testing golang power
 */
+
+//TODO Refactor code for error handling and make readable structure
+
 package main
 
 import (
 	"fmt"
-//	"io"
+	"bufio"
+//	"io/ioutil"
 	"log"
+	"path/filepath"
 	"net/http"
-//	"os"
+	"os"
 	"encoding/json"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"strings"
 )
 
 func doQuote() string {
-    return "some quote"
+	resp, err := http.Get("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var quoteData []interface{}
+	err = json.NewDecoder(resp.Body).Decode(&quoteData)
+	if err != nil {
+		log.Fatalln(err)
+		return "Sorry no quotes :|"
+	}
+	fmt.Printf("Your quote data %v", quoteData)
+	quoteContent := quoteData[0].(map[string]interface{})["content"].(string)
+	quoteContent = strings.Replace(quoteContent, "<p>", "", -1)
+	quoteContent = strings.Replace(quoteContent, "</p>", "", -1)
+	quoteContent = strings.TrimSuffix(quoteContent, "\n")
+	quoteContent = strings.TrimSuffix(quoteContent, " ")
+	quoteAuthor := quoteData[0].(map[string]interface{})["title"].(string)
+	message := "\"" + quoteContent + "\"\n" + quoteAuthor
+	return message
+	
 }
-
 func parseText(webhookData map[string]interface{}) string {
 	events := webhookData["events"].([]interface{})
 	event := events[0].(map[string]interface{})
@@ -39,6 +64,50 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	}
 	text := parseText(webhookData)
 	fmt.Printf("Your message is: %s\n", text)
+	var replyMessage string
+	if(text == "/quote") {
+		replyMessage = doQuote()
+	} else if(text == "/help" || text == "/h") {
+		replyMessage = "Hello.\nMy name is Butter. I am an intelligent bot, " +
+					"but I was created for the simple tasks. Currently, I support following commands:\n" +
+					"/quote - give a random quote"
+	} else {
+		return
+	}
+	// get secrets
+	var channel_secret, channel_token string
+	file_path, err := filepath.Abs("./.bot_configs")
+	file, err := os.Open(file_path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		if (channel_secret == "") {
+			channel_secret = scanner.Text()
+		} else {
+			channel_token = scanner.Text()
+		}
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+	// reply
+	bot, err := linebot.New(channel_secret, channel_token)
+	if err != nil {
+		fmt.Println("Failed to create bot channel\n")
+		return
+	}
+	events := webhookData["events"].([]interface{})
+	event := events[0].(map[string]interface{})
+	replyToken := event["replyToken"].(string)
+	if _, err := bot.ReplyMessage(replyToken, linebot.NewTextMessage(replyMessage)).Do(); 
+	err != nil {
+		fmt.Println("Failed to reply\n")
+	}
 }
 
 func main() {
