c49d0cb92c54d95c0f9160bb332cdefe98eeb93e
(학생) ERKIN MATKAZIEV (전기전자컴퓨터공학부) <erkin97@unist.ac.kr>
2019-04-15 10:46:02 +0900

    organize

--- Line/main.go
+++ Line/main.go
@@ -1,39 +1,35 @@
-/*
-simple webhook server for my bot
-this spaghetti code just for testing golang power
-*/
-
-//TODO Refactor code for error handling and make readable structure
-
 package main
 
 import (
-	"fmt"
 	"bufio"
-//	"io/ioutil"
+	"encoding/json"
 	"html"
 	"log"
-	"path/filepath"
-	"github.com/grokify/html-strip-tags-go"
 	"net/http"
 	"os"
-	"encoding/json"
-	"github.com/line/line-bot-sdk-go/linebot"
+	"path/filepath"
 	"strings"
+
+	strip "github.com/grokify/html-strip-tags-go"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-func doQuote() string {
+func getQuote() string {
 	resp, err := http.Get("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1")
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf("Failed to request quote. Error: %v\n", err)
+		return ""
 	}
+
 	var quoteData []interface{}
 	err = json.NewDecoder(resp.Body).Decode(&quoteData)
 	if err != nil {
-		log.Fatalln(err)
-		return "Sorry no quotes :|"
+		log.Printf("Failed to request quote. Error: %v\n", err)
+		return ""
 	}
-	fmt.Printf("Your quote data %v", quoteData)
+
+	log.Printf("Your quote data %v\n", quoteData)
+
 	quoteContent := quoteData[0].(map[string]interface{})["content"].(string)
 	quoteContent = strip.StripTags(quoteContent)
 	quoteContent = html.UnescapeString(quoteContent)
@@ -41,8 +37,8 @@ func doQuote() string {
 	quoteContent = strings.TrimSuffix(quoteContent, " ")
 	quoteAuthor := quoteData[0].(map[string]interface{})["title"].(string)
 	message := "\"" + quoteContent + "\"\n" + quoteAuthor
+
 	return message
-	
 }
 func parseText(webhookData map[string]interface{}) string {
 	events := webhookData["events"].([]interface{})
@@ -59,61 +55,61 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	// log webhook payload
-	fmt.Println("got webhook payload: ")
+	log.Println("got webhook payload: ")
 	for k, v := range webhookData {
-		fmt.Printf("%s : %v\n", k, v)
+		log.Printf("%s : %v\n", k, v)
 	}
 	text := parseText(webhookData)
-	fmt.Printf("Your message is: %s\n", text)
+	log.Printf("Your message is: %s\n", text)
+
 	var replyMessage string
-	if(text == "/quote") {
-		replyMessage = doQuote()
-	} else if(text == "/help" || text == "/h") {
+	if text == "/quote" {
+		replyMessage = getQuote()
+	} else if text == "/help" || text == "/h" {
 		replyMessage = "Hello.\nMy name is Butter. I am an intelligent bot, " +
-					"but I was created for the simple tasks. Currently, I support following commands:\n" +
-					"/quote - give a random quote"
+			"but I was created for the simple tasks. Currently, I support following commands:\n" +
+			"/quote - give a random quote"
 	} else {
 		return
 	}
+
 	// get secrets
-	var channel_secret, channel_token string
-	file_path, err := filepath.Abs("./.bot_configs")
-	file, err := os.Open(file_path)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-		if (channel_secret == "") {
-			channel_secret = scanner.Text()
-		} else {
-			channel_token = scanner.Text()
-		}
-    }
-
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-	// reply
-	bot, err := linebot.New(channel_secret, channel_token)
+	var channelSecret, channelToken string
+	filePath, err := filepath.Abs("./.bot_configs")
+	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("Failed to create bot channel\n")
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if channelSecret == "" {
+			channelSecret = scanner.Text()
+		} else {
+			channelSecret = scanner.Text()
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	// reply
+	bot, err := linebot.New(channelSecret, channelToken)
+	if err != nil {
+		log.Println("Failed to create bot channel")
 		return
 	}
 	events := webhookData["events"].([]interface{})
 	event := events[0].(map[string]interface{})
 	replyToken := event["replyToken"].(string)
-	if _, err := bot.ReplyMessage(replyToken, linebot.NewTextMessage(replyMessage)).Do(); 
-	err != nil {
-		fmt.Println("Failed to reply\n")
+	if _, err := bot.ReplyMessage(replyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+		log.Println("Failed to reply")
 	}
 }
 
 func main() {
-    log.Println("server started")
+	log.Println("server started")
 	http.HandleFunc("/", handleWebhook)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
